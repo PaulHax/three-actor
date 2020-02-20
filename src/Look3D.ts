a0a8ea36708f877e347a49bc5bf2c95b1fec9c68
Paul <gladsomebeast@gmail.com>
2020-02-20 11:22:24 -0800

    refactoring to curry or partal application2

--- src/Look3D.ts
+++ src/Look3D.ts
@@ -1,15 +1,23 @@
-import { Bone } from 'three';
+import { Bone, Vector3 } from 'three';
 
 export interface Look3D {
   neck: Bone;
 }
 
-export function makeLook3D(neck: Bone): Look3D {
+export function makeLook3D(target: Vector3, neck: Bone): Look3D {
   return {
     neck: neck //side effect
   };
 }
 
-export function makeTick(view: Look3D): Function {
-  return () => {};
+// export function makeTick(view: Look3D): Function {
+//   return () => {
+//     view.neck.lookAt();
+//   };
+// }
+
+export function look3D(neck: Bone): Function {
+  return (target: Vector3): void => {
+    neck.lookAt(target);
+  };
 }
--- test/Look3D.spec.ts
+++ test/Look3D.spec.ts
@@ -1,12 +1,33 @@
+import * as THREE from 'three';
 import { makeLook3D, makeTick } from '../src/Look3D';
 
 test('constructs', () => {
-  const view = makeLook3D();
+  const neckBone = new THREE.Bone();
+  const view = makeLook3D(neckBone);
   expect(view).toBeTruthy();
 });
 
 test('View3D ticks', () => {
-  const view = makeLook3D();
+  const neckBone = new THREE.Bone();
+  const view = makeLook3D(neckBone);
   const tick = makeTick(view);
   expect(tick).toBeTruthy();
+});
+
+test('Neck looks at target', () => {
+  const target = new THREE.Vector3(0.2, 0.3, 5);
+  const neckBone = new THREE.Bone();
+  const view = makeLook3D(neckBone);
+  const tick = makeTick(view);
+  tick();
+  // neckBone.updateWorldMatrix(true, false); //?
+  const angleToTarget = new THREE.Vector3(0, 0, 1)
+    .applyMatrix4(neckBone.matrixWorld) //get forward vector
+    .angleTo(
+      //Neck to target vector
+      target.sub(
+        new THREE.Vector3().setFromMatrixPosition(neckBone.matrixWorld)
+      )
+    );
+  expect(angleToTarget).toBeLessThan(0.01);
 });
